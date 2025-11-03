bef61091e0224ac53d16ca6fade28cf95c4d95e6
Cristiano Silva <cgts.br@gmail.com>
2025-11-03 18:12:33 -0300

    Teste

--- teste/obj/Release/net7.0/teste.AssemblyInfo.cs
+++ teste/obj/Release/net7.0/teste.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("primeiro")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("teste")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493a175789ec417b4ad00f713e8c466ab4f8a1f5")]
-[assembly: System.Reflection.AssemblyProductAttribute("primeiro")]
-[assembly: System.Reflection.AssemblyTitleAttribute("primeiro")]
+[assembly: System.Reflection.AssemblyProductAttribute("teste")]
+[assembly: System.Reflection.AssemblyTitleAttribute("teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
